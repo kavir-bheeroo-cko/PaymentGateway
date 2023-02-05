368e37f73d19a450aa5c5adff0abfa113d980939
Kavir Bheeroo <kavir.bheeroo@checkout.com>
2023-02-05 20:41:42 +0000

    Added Payment Handler

---
+++ src/PaymentGateway.Api/Payments/PaymentHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using OneOf;
+
+namespace PaymentGateway.Api.Payments;
+
+public class PaymentHandler : IRequestHandler<PaymentRequest,  OneOf<PaymentResponse, Exception>>
+{
+    public PaymentHandler()
+    {
+
+    }
+
+    public async Task<OneOf<PaymentResponse, Exception>> Handle(PaymentRequest request, CancellationToken cancellationToken)
+    {
+        // validate
+
+        // send to bank
+
+        // create payment
+
+        // store in DB
+
+        // dispatch event
+
+        // build response
+
+        throw new NotImplementedException();
+    }
+}
