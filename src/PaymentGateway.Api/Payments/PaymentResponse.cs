4a45131279dbb0b22bc5ad859600aaea86d44260
Kavir Bheeroo <kavir.bheeroo@checkout.com>
2023-02-05 20:41:05 +0000

    Added payment DTOs

---
+++ src/PaymentGateway.Api/Payments/PaymentResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace PaymentGateway.Api.Payments;
+
+public class PaymentResponse
+{
+    public Guid Id { get; }
+
+    public string Status { get; }
+
+    private PaymentResponse(string status)
+    {
+        Id = Guid.NewGuid();
+        Status = status;
+    }
+
+    public PaymentResponse BuildAuthorizedResponse() => new (PaymentStatus.Authorized);
+
+    public PaymentResponse BuildDeclinedResponse() => new(PaymentStatus.Declined);
+}
