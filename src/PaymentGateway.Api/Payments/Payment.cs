aad76d97d567b47106b5e195e89bdfffeff062a3
Kavir Bheeroo <kavir.bheeroo@checkout.com>
2023-02-05 20:41:27 +0000

    Added Payment domain object

---
+++ src/PaymentGateway.Api/Payments/Payment.cs
@@ -0,0 +1,45 @@
+﻿namespace PaymentGateway.Api.Payments;
+
+public class Payment
+{
+    public Guid Id { get; }
+
+    public Card Card { get; }
+
+    public uint Amount { get; }
+
+    public string Currency { get; }
+
+    public string Status { get; }
+
+    public string AuthorizationCode { get; }
+
+    public string AcquirerName { get; }
+
+    public Payment(Guid id, Card card, uint amount, string currency, string status, string authorizationCode, string acquirerName)
+    {
+        Id = id;
+        Card = card;
+        Amount = amount;
+        Currency = currency;
+        Status = status;
+        AuthorizationCode = authorizationCode;
+        AcquirerName = acquirerName;
+    }
+}
+
+public class Card
+{
+    public string Number { get; }
+
+    public int ExpiryMonth { get; }
+
+    public int ExpiryYear { get; }
+
+    public Card(string number, int expiryMonth, int expiryYear)
+    {
+        Number = number;
+        ExpiryMonth = expiryMonth;
+        ExpiryYear = expiryYear;
+    }
+}
