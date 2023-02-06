138625f44b8e30061db69a9eaf9e7c2a97c7614b
Kavir Bheeroo <kavir.bheeroo@checkout.com>
2023-02-06 18:43:27 +0000

    Added extensions class

---
+++ src/PaymentGateway.Api/Payments/PaymentExtensions.cs
@@ -0,0 +1,17 @@
+﻿using PaymentGateway.Api.Payments.Commands;
+
+namespace PaymentGateway.Api.Payments
+{
+    public static class PaymentExtensions
+    {
+        public static PaymentEvent ToEvent(this Payment payment) =>
+            new(
+                payment.Id,
+                payment.Card.Number[^4..],
+                payment.Card.ExpiryMonth,
+                payment.Card.ExpiryYear,
+                payment.Amount,
+                payment.Currency,
+                payment.Status);
+    }
+}
