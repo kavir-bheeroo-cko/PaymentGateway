3d19ed976ebc3daa2a4e4eba7a611ec56a578033
Kavir Bheeroo <kavir.bheeroo@checkout.com>
2023-02-06 00:03:30 +0000

    Added PaymentEventHandler and PaymentEvent model

---
+++ src/PaymentGateway.Api/Payments/PaymentEvent.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace PaymentGateway.Api.Payments
+{
+    public class PaymentEvent : INotification
+    {
+        public Guid Id { get; }
+
+        public PaymentEvent(Payment payment)
+        {
+            Id = payment.Id;    
+        }
+    }
+}
